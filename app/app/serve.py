e6713079f4a5fe6b5f602810aadf208d77f20633
Alexander Lockshyn <info@lab37.com>
2017-12-20 19:26:15 +0200

    Disabled old python bitcoin and ethereum clients, switching to new ones and python 3

--- app/app/serve.py
+++ app/app/serve.py
@@ -1,5 +1,5 @@
-from eth_client import c
-from btc_client import BitcoinClient
+# from eth_client import c
+# from btc_client import BitcoinClient
 from flask import Flask
 from flask import jsonify
 
@@ -7,18 +7,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    bc = BitcoinClient()
-    bitcoin_info = bc.info()
-
-    ethereum_info = {
-        "net_version": c.net_version(),
-        "web3_client_version": c.web3_clientVersion(),
-        "gas_price": c.eth_gasPrice(),
-        "block_number": c.eth_blockNumber()
-    }
-
-    resp = {"bitcoin": bitcoin_info, "ethereum": ethereum_info}
-    resp["bitcoin"]["balance"] = bc.balance()
+    # bc = BitcoinClient()
+    # bitcoin_info = bc.info()
+    #
+    # ethereum_info = {
+    #     "net_version": c.net_version(),
+    #     "web3_client_version": c.web3_clientVersion(),
+    #     "gas_price": c.eth_gasPrice(),
+    #     "block_number": c.eth_blockNumber()
+    # }
+    #
+    # resp = {"bitcoin": bitcoin_info, "ethereum": ethereum_info}
+    # resp["bitcoin"]["balance"] = bc.balance()
+    resp = {"bitcoin": "blah-blah", "ethereum": "blah-blah"}
     return jsonify(resp)
 
 if __name__ == '__main__':
