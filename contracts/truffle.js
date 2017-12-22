0d64765ff9edcc3c08ca6441ca29d066fa12b5cd
Alexander Lockshyn <info@lab37.com>
2017-12-22 14:41:09 +0200

    Fixing docker image for contracts

--- contracts/truffle.js
+++ contracts/truffle.js
@@ -1,3 +1,8 @@
+//var Web3 = require('web3');
+//var web3 = new Web3(new Web3.providers.HttpProvider('http://' + process.env.RPC_HOST + ':' + process.env.RPC_PORT));
+//console.log('>> Unlocking account ');
+//web3.personal.unlockAccount(process.env.DEPLOYER_ADDRESS, process.env.DEPLOYER_PASSPHRASE, 36000);
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,7 +12,7 @@ module.exports = {
         port: process.env.RPC_PORT || 8545,
         network_id: "*",
         gas: 4612388,
-        from: process.env.DEPLOYER_ADDRESS
+        from: process.env.DEPLOYER_ADDRESS,
       }
   }
 };
