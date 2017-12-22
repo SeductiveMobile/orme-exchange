04815e976b7832c69c41b8ebc3ef4ad3c200e294
Alexander Lockshyn <info@lab37.com>
2017-12-22 13:24:45 +0200

    Added deploiyer credentials to truffle

--- contracts/truffle.js
+++ contracts/truffle.js
@@ -6,7 +6,8 @@ module.exports = {
         host: process.env.RPC_HOST || "localhost",
         port: process.env.RPC_PORT || 8545,
         network_id: "*",
-        gas: 4612388
+        gas: 4612388,
+        from: process.env.DEPLOYER_ADDRESS
       }
   }
 };
